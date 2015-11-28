755f6c5e18fd26b004f0437d6ab495cbc3d69af7
Robin Haveneers <haveneersrobin@gmail.com>
2015-11-28 16:12:58 +0100

    Added start and finish. Refined script. Needs refactoring.

--- Onderzoek/Chromatic Maze/Scripts/parse_data_to_json.js
+++ Onderzoek/Chromatic Maze/Scripts/parse_data_to_json.js
@@ -5,14 +5,31 @@ fs.readFile('Scripts/data.txt', function(err, data) {
     if(err) throw err;
     var array = data.toString().split("\n");
     for (i=0; i<array.length - 1; i++) {
-      var xco = array[i].substring(5,6);
-      var yco = array[i].substring(7,8);
-      var color = array[i].slice(9, array[i].indexOf(")"));
-      dict.push({
-        x: xco,
-        y : yco,
-        color: color,
-      })
+      if(array[i].substring(0,1) !== "s") {
+        var tile = array[i].substring(0,4);
+        var xco = array[i].substring(5,6);
+        var yco = array[i].substring(7,8);
+        var color = array[i].slice(9, array[i].indexOf(")"));
+        dict.push({
+          name:tile,
+          x : xco,
+          y : yco,
+          color: color,
+        })
+      }
+      else {
+        var sxco = array[i].substring(17,18);
+        var syco = array[i].substring(19,20);
+        var fxco = array[i].substring(21,22);
+        var fyco = array[i].substring(23,24);
+        dict.push({
+          name:"s-f",
+          sx : sxco,
+          sy : syco,
+          fx : fxco,
+          fy : fyco,
+        })
+      }
     }
     fs.writeFile("Scripts/parsed_data.js", JSON.stringify(dict), function(err) {
         if(err) {
--- Onderzoek/Chromatic Maze/chromatic-maze.pl
+++ Onderzoek/Chromatic Maze/chromatic-maze.pl
@@ -1,253 +1,41 @@
-(1/6) :: tile(1,1, red);
-(1/6) :: tile(1,1, yellow);
-(1/6) :: tile(1,1, green);
-(1/6) :: tile(1,1, cyan);
-(1/6) :: tile(1,1, blue);
-(1/6) :: tile(1,1, magenta).
+(1/6) :: tile(X,Y,red);
+(1/6) :: tile(X,Y,yellow);
+(1/6) :: tile(X,Y,green);
+(1/6) :: tile(X,Y,cyan);
+(1/6) :: tile(X,Y,blue);
+(1/6) :: tile(X,Y,magenta).
 
-(1/6) :: tile(1,2, red);
-(1/6) :: tile(1,2, yellow);
-(1/6) :: tile(1,2, green);
-(1/6) :: tile(1,2, cyan);
-(1/6) :: tile(1,2, blue);
-(1/6) :: tile(1,2, magenta).
+(1/6) :: sxco(1);
+(1/6) :: sxco(2);
+(1/6) :: sxco(3);
+(1/6) :: sxco(4);
+(1/6) :: sxco(5);
+(1/6) :: sxco(6).
 
-(1/6) :: tile(1,3, red);
-(1/6) :: tile(1,3, yellow);
-(1/6) :: tile(1,3, green);
-(1/6) :: tile(1,3, cyan);
-(1/6) :: tile(1,3, blue);
-(1/6) :: tile(1,3, magenta).
+(1/6) :: syco(1);
+(1/6) :: syco(2);
+(1/6) :: syco(3);
+(1/6) :: syco(4);
+(1/6) :: syco(5);
+(1/6) :: syco(6).
 
-(1/6) :: tile(1,4, red);
-(1/6) :: tile(1,4, yellow);
-(1/6) :: tile(1,4, green);
-(1/6) :: tile(1,4, cyan);
-(1/6) :: tile(1,4, blue);
-(1/6) :: tile(1,4, magenta).
+(1/6) :: fxco(1);
+(1/6) :: fxco(2);
+(1/6) :: fxco(3);
+(1/6) :: fxco(4);
+(1/6) :: fxco(5);
+(1/6) :: fxco(6).
 
-(1/6) :: tile(1,5, red);
-(1/6) :: tile(1,5, yellow);
-(1/6) :: tile(1,5, green);
-(1/6) :: tile(1,5, cyan);
-(1/6) :: tile(1,5, blue);
-(1/6) :: tile(1,5, magenta).
+(1/6) :: fyco(1);
+(1/6) :: fyco(2);
+(1/6) :: fyco(3);
+(1/6) :: fyco(4);
+(1/6) :: fyco(5);
+(1/6) :: fyco(6).
 
-(1/6) :: tile(1,6, red);
-(1/6) :: tile(1,6, yellow);
-(1/6) :: tile(1,6, green);
-(1/6) :: tile(1,6, cyan);
-(1/6) :: tile(1,6, blue);
-(1/6) :: tile(1,6, magenta).
+start_and_finish(Sx,Sy,Fx,Fy) :- Sx \== Fx,
+                                 Sy \== Fy,
+                                 sxco(Sx),syco(Sy),fxco(Fx), fyco(Fy).
 
-(1/6) :: tile(2,1, red);
-(1/6) :: tile(2,1, yellow);
-(1/6) :: tile(2,1, green);
-(1/6) :: tile(2,1, cyan);
-(1/6) :: tile(2,1, blue);
-(1/6) :: tile(2,1, magenta).
-
-(1/6) :: tile(2,2, red);
-(1/6) :: tile(2,2, yellow);
-(1/6) :: tile(2,2, green);
-(1/6) :: tile(2,2, cyan);
-(1/6) :: tile(2,2, blue);
-(1/6) :: tile(2,2, magenta).
-
-(1/6) :: tile(2,3, red);
-(1/6) :: tile(2,3, yellow);
-(1/6) :: tile(2,3, green);
-(1/6) :: tile(2,3, cyan);
-(1/6) :: tile(2,3, blue);
-(1/6) :: tile(2,3, magenta).
-
-(1/6) :: tile(2,4, red);
-(1/6) :: tile(2,4, yellow);
-(1/6) :: tile(2,4, green);
-(1/6) :: tile(2,4, cyan);
-(1/6) :: tile(2,4, blue);
-(1/6) :: tile(2,4, magenta).
-
-(1/6) :: tile(2,5, red);
-(1/6) :: tile(2,5, yellow);
-(1/6) :: tile(2,5, green);
-(1/6) :: tile(2,5, cyan);
-(1/6) :: tile(2,5, blue);
-(1/6) :: tile(2,5, magenta).
-
-(1/6) :: tile(2,6, red);
-(1/6) :: tile(2,6, yellow);
-(1/6) :: tile(2,6, green);
-(1/6) :: tile(2,6, cyan);
-(1/6) :: tile(2,6, blue);
-(1/6) :: tile(2,6, magenta).
-
-(1/6) :: tile(3,1, red);
-(1/6) :: tile(3,1, yellow);
-(1/6) :: tile(3,1, green);
-(1/6) :: tile(3,1, cyan);
-(1/6) :: tile(3,1, blue);
-(1/6) :: tile(3,1, magenta).
-
-(1/6) :: tile(3,2, red);
-(1/6) :: tile(3,2, yellow);
-(1/6) :: tile(3,2, green);
-(1/6) :: tile(3,2, cyan);
-(1/6) :: tile(3,2, blue);
-(1/6) :: tile(3,2, magenta).
-
-(1/6) :: tile(3,3, red);
-(1/6) :: tile(3,3, yellow);
-(1/6) :: tile(3,3, green);
-(1/6) :: tile(3,3, cyan);
-(1/6) :: tile(3,3, blue);
-(1/6) :: tile(3,3, magenta).
-
-(1/6) :: tile(3,4, red);
-(1/6) :: tile(3,4, yellow);
-(1/6) :: tile(3,4, green);
-(1/6) :: tile(3,4, cyan);
-(1/6) :: tile(3,4, blue);
-(1/6) :: tile(3,4, magenta).
-
-(1/6) :: tile(3,5, red);
-(1/6) :: tile(3,5, yellow);
-(1/6) :: tile(3,5, green);
-(1/6) :: tile(3,5, cyan);
-(1/6) :: tile(3,5, blue);
-(1/6) :: tile(3,5, magenta).
-
-(1/6) :: tile(3,6, red);
-(1/6) :: tile(3,6, yellow);
-(1/6) :: tile(3,6, green);
-(1/6) :: tile(3,6, cyan);
-(1/6) :: tile(3,6, blue);
-(1/6) :: tile(3,6, magenta).
-
-(1/6) :: tile(4,1, red);
-(1/6) :: tile(4,1, yellow);
-(1/6) :: tile(4,1, green);
-(1/6) :: tile(4,1, cyan);
-(1/6) :: tile(4,1, blue);
-(1/6) :: tile(4,1, magenta).
-
-(1/6) :: tile(4,2, red);
-(1/6) :: tile(4,2, yellow);
-(1/6) :: tile(4,2, green);
-(1/6) :: tile(4,2, cyan);
-(1/6) :: tile(4,2, blue);
-(1/6) :: tile(4,2, magenta).
-
-(1/6) :: tile(4,3, red);
-(1/6) :: tile(4,3, yellow);
-(1/6) :: tile(4,3, green);
-(1/6) :: tile(4,3, cyan);
-(1/6) :: tile(4,3, blue);
-(1/6) :: tile(4,3, magenta).
-
-(1/6) :: tile(4,4, red);
-(1/6) :: tile(4,4, yellow);
-(1/6) :: tile(4,4, green);
-(1/6) :: tile(4,4, cyan);
-(1/6) :: tile(4,4, blue);
-(1/6) :: tile(4,4, magenta).
-
-(1/6) :: tile(4,5, red);
-(1/6) :: tile(4,5, yellow);
-(1/6) :: tile(4,5, green);
-(1/6) :: tile(4,5, cyan);
-(1/6) :: tile(4,5, blue);
-(1/6) :: tile(4,5, magenta).
-
-(1/6) :: tile(4,6, red);
-(1/6) :: tile(4,6, yellow);
-(1/6) :: tile(4,6, green);
-(1/6) :: tile(4,6, cyan);
-(1/6) :: tile(4,6, blue);
-(1/6) :: tile(4,6, magenta).
-
-(1/6) :: tile(5,1, red);
-(1/6) :: tile(5,1, yellow);
-(1/6) :: tile(5,1, green);
-(1/6) :: tile(5,1, cyan);
-(1/6) :: tile(5,1, blue);
-(1/6) :: tile(5,1, magenta).
-
-(1/6) :: tile(5,2, red);
-(1/6) :: tile(5,2, yellow);
-(1/6) :: tile(5,2, green);
-(1/6) :: tile(5,2, cyan);
-(1/6) :: tile(5,2, blue);
-(1/6) :: tile(5,2, magenta).
-
-(1/6) :: tile(5,3, red);
-(1/6) :: tile(5,3, yellow);
-(1/6) :: tile(5,3, green);
-(1/6) :: tile(5,3, cyan);
-(1/6) :: tile(5,3, blue);
-(1/6) :: tile(5,3, magenta).
-
-(1/6) :: tile(5,4, red);
-(1/6) :: tile(5,4, yellow);
-(1/6) :: tile(5,4, green);
-(1/6) :: tile(5,4, cyan);
-(1/6) :: tile(5,4, blue);
-(1/6) :: tile(5,4, magenta).
-
-(1/6) :: tile(5,5, red);
-(1/6) :: tile(5,5, yellow);
-(1/6) :: tile(5,5, green);
-(1/6) :: tile(5,5, cyan);
-(1/6) :: tile(5,5, blue);
-(1/6) :: tile(5,5, magenta).
-
-(1/6) :: tile(5,6, red);
-(1/6) :: tile(5,6, yellow);
-(1/6) :: tile(5,6, green);
-(1/6) :: tile(5,6, cyan);
-(1/6) :: tile(5,6, blue);
-(1/6) :: tile(5,6, magenta).
-
-(1/6) :: tile(6,1, red);
-(1/6) :: tile(6,1, yellow);
-(1/6) :: tile(6,1, green);
-(1/6) :: tile(6,1, cyan);
-(1/6) :: tile(6,1, blue);
-(1/6) :: tile(6,1, magenta).
-
-(1/6) :: tile(6,2, red);
-(1/6) :: tile(6,2, yellow);
-(1/6) :: tile(6,2, green);
-(1/6) :: tile(6,2, cyan);
-(1/6) :: tile(6,2, blue);
-(1/6) :: tile(6,2, magenta).
-
-(1/6) :: tile(6,3, red);
-(1/6) :: tile(6,3, yellow);
-(1/6) :: tile(6,3, green);
-(1/6) :: tile(6,3, cyan);
-(1/6) :: tile(6,3, blue);
-(1/6) :: tile(6,3, magenta).
-
-(1/6) :: tile(6,4, red);
-(1/6) :: tile(6,4, yellow);
-(1/6) :: tile(6,4, green);
-(1/6) :: tile(6,4, cyan);
-(1/6) :: tile(6,4, blue);
-(1/6) :: tile(6,4, magenta).
-
-(1/6) :: tile(6,5, red);
-(1/6) :: tile(6,5, yellow);
-(1/6) :: tile(6,5, green);
-(1/6) :: tile(6,5, cyan);
-(1/6) :: tile(6,5, blue);
-(1/6) :: tile(6,5, magenta).
-
-(1/6) :: tile(6,6, red);
-(1/6) :: tile(6,6, yellow);
-(1/6) :: tile(6,6, green);
-(1/6) :: tile(6,6, cyan);
-(1/6) :: tile(6,6, blue);
-(1/6) :: tile(6,6, magenta).
-
-query(tile(_,_,C)).
+query(tile(X,Y,C)) :- between(1,6,X), between(1,6,Y).
+query(start_and_finish(A,B,C,D)).
